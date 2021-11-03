6569669392bc4d92d5590cbc4dfb0bf27a82b9ef
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-11-03 16:35:20 +0000

    Create Collaborate Service class

---
+++ collaborate/service.py
@@ -0,0 +1,10 @@
+from .member import Member
+
+class CollaborateService:
+    """
+    """
+    def __init__(self) -> None:
+        self.member: dict[str, Member] = {}
+
+    def create_matches() -> None:
+        pass
