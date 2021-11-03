e49c51cd222435ae2c7609d991bbb46733723d89
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-11-03 16:17:25 +0000

    Define constants and load datasets

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+import pathlib
 import pandas as pd
 import random
 
 
+# Config
+AUTHOR = "elliott.phillips@ons.gov.uk"
+DATA_DIR = pathlib.Path("data/")
+SIGNUP_DATA = DATA_DIR / "signups.csv"
+PREVIOUS_MATCHES = DATA_DIR / "previous_matches.csv"
+
+
 def main():
-    pass
+    # Load datasets
+    collaborate_data = pd.read_csv(SIGNUP_DATA)
+    previous_matches = pd.read_csv(PREVIOUS_MATCHES, encoding="utf-8")
+
+    print(collaborate_data.head())
+    print(previous_matches.head())
 
 
 if __name__ =="__main__":
