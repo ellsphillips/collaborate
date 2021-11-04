fb8a9a1db703fe644c783abc0fad7039df123389
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-11-04 13:45:33 +0000

    Allow Member hashing for comparison

--- collaborate/member.py
+++ collaborate/member.py
@@ -12,17 +12,22 @@ class Division(Enum):
     EP = auto()
 
 
-@dataclass
+@dataclass(order=True, frozen=True, eq=True)
 class Member:
-    member_id: str = field(repr=False)
+    member_id: str = field(
+        repr=True,
+        compare=False,
+        hash=False
+    )
     name: str
     email: str
     division: Division
 
     def __str__(self) -> str:
         return (
-          f"{Colour.CYAN}Member: "
-          f"{Colour.END}{self.division} \t"
-          f"{Colour.YELLOW}{self.name} "
-          f"{Colour.END}({self.email})"
+            f"{Colour.PURPLE}{self.member_id}"
+            f"{Colour.CYAN} Member: "
+            f"{Colour.END}{self.division} \t"
+            f"{Colour.YELLOW}{self.name} "
+            f"{Colour.END}({self.email})"
         )
