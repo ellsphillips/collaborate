975be05a21f00c515cb8ffe6b4bf7bcade750ab2
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-11-04 13:45:55 +0000

    Remove duplicates method for Matcher

--- collaborate/match.py
+++ collaborate/match.py
@@ -1,6 +1,21 @@
+from .member import Member
+
 class Matcher:
     """
     """
 
     def __init__(self) -> None:
         pass
+
+    def remove_duplicates(self, member_list: list[Member]) -> None:
+        if len(member_list) == len(set(member_list)):
+            return member_list
+        
+        duplicates = set([m for m in member_list if member_list.count(m) > 1])
+        print(
+            "\n".join([
+                f"Removed {len(duplicates)} duplicates from " \
+                    f"list of {len(member_list)} members:",
+                *sorted([str(dup) for dup in duplicates])
+            ])
+        )
