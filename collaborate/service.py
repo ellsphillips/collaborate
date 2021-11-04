711b974ff91fd01110a1313497ae17917cc87dad
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-11-04 10:27:01 +0000

    ID generator for Member objects

--- collaborate/service.py
+++ collaborate/service.py
@@ -1,4 +1,12 @@
+import random
+import string
+from pandas import DataFrame
+
 from .member import Member
+
+
+def generate_id(length: int = 32):
+    return "".join(random.choices(string.ascii_uppercase, k=length))
 
 class CollaborateService:
     """
