31fb617144b9f164cca2b03e70b457f651c8dbdb
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-11-15 15:14:27 +0000

    DB data setter and getter

--- collaborate/database.py
+++ collaborate/database.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 import json
 
@@ -8,10 +8,21 @@ class Database:
     """Member database manager"""
 
     file_path: Path
+    __data: dict = field(default_factory=dict)
+
+    @property
+    def data(self) -> str:
+        return self.__data
+
+    @data.setter
+    def data(self, db_obj: dict) -> None:
+        self.__data = db_obj
 
     def load(self) -> dict:
         with open(self.file_path) as f:
             data = json.load(f)
+
+        self.data = data
             
         return data
         
