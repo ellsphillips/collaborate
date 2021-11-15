28a02e719e5403fe03af734382f6c6fe9ed2564e
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-11-15 11:21:54 +0000

    XD pair reducer and matching report

--- collaborate/match.py
+++ collaborate/match.py
@@ -41,16 +41,19 @@ class XDMatchingStrategy(MatchingStrategy):
         threshold: int
     ) -> None:
         while not len(members) < 2:
-            
-            first, *members = random.sample(members, len(members) - 2)
+            first = random.choice(members)
+            members.remove(first)
 
+            punt = None
             for _ in range(threshold):
-                second = random.choice(members)
-                if first.division.name != second.division.name:
-                    members.remove(second)
-                    return first, second
+                punt = random.choice(members)
+                if first.division.name != punt.division.name:
+                    members.remove(punt)
+                    break
 
-            return first, random.sample(members, 1)[0]
+            second = punt if punt is not None else random.sample(members, 1)[0]
+
+            yield [first, second]
         
 class BlackHoleMatchingStrategy(MatchingStrategy):
     """Chuck it in the bin"""
@@ -71,33 +74,30 @@ class Matcher:
         
         duplicates = set([
             m for m in self.members
-            if self.members.count(m) > 1]
-        )
+            if self.members.count(m) > 1
+        ])
 
         print(
-            "\n".join([
-                f"Removed {len(duplicates)} duplicates from " \
-                    f"list of {len(self.members)} members:",
-                *sorted([str(dup) for dup in duplicates])
-            ])
+            f"Removed {len(duplicates)} duplicates from " \
+                f"list of {len(self.members)} members:",
+            *sorted([str(dup) for dup in duplicates]),
+            "\n",
+            sep="\n"
         )
 
-        return sorted(list(set(self.members)))
+        self.members = sorted(set(self.members))
 
-    def ensure_even(self, author: str) -> None:
+    def ensure_even(self, author: Member) -> None:
         if len(self.members) % 2 != 0:
             self.members.remove(author)
 
     def match_members(self, matching_strategy: MatchingStrategy) -> None:
-        print(
-            "\nMatching members with",
-            f"{Colour.CYAN}{matching_strategy.__class__.__name__}{Colour.END}",
-            "\n" + "-"*50
-        )
-        
         matches = matching_strategy.create_matches(self.members)
         
         print(
+            "Matching members with"
+            f"{Colour.CYAN}{matching_strategy.__class__.__name__}{Colour.END}",
+            "-"*50,
             *[
                 "\t->\t".join([
                     first.division.name + "\t" + first.email,
